2ab58651a42d4fc1faeee383c1fea93de035b63d
ratking114 <almeidafrancisco935@gmail.com>
2022-11-10 16:18:41 +0000

    Finished server-side login

--- src/server/database/EventDatabase.java
+++ src/server/database/EventDatabase.java
@@ -6,9 +6,7 @@ import shared.User;
 import shared.UserViewModel;
 
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * EventDatabase is the server.database that stores the events and all information
@@ -27,13 +25,58 @@ public class EventDatabase {
         );
     }
 
-    public Error_Messages login(Message received_login){
-        UserViewModel user_viewmodel = new UserViewModel();
+    /**
+     * Receives a message with a UserViewModel in the attachment field.
+     * <p>
+     * This function queries the database for the username field contained inside the UserViewModel which contains a User,
+     * if the username exists it will compare the hashed passwords.
+     * <p>
+     * Returns the user data in case of a sucessfull login (name,password,administrator,password etc).
+     * In case of a failed login due to password mismatch returns null along with an Error_Messages.INVALID_PASSWORD
+     * If it doesn't find the username in the Database it will return null along with an Error_Messages.USERNAME_DOESNT_EXIST
+     */
+    public Message login(Message received_login) throws SQLException {
         User login_user = (User) received_login.attachment;
 
-        //TODO(see if the username and password match the ones in the DB i have no idea hhow plase help :( )
+        String query = "select * from utilizador where username=?";
 
-        return Error_Messages.SUCESS;
+        //create a prepared statement and add the received username to it
+        PreparedStatement statement = database_connection.prepareStatement(query);
+        statement.setString(1,login_user.username);
+
+        //execute the query and save the results in a ResultSet
+        ResultSet query_result = statement.executeQuery();
+
+
+        //if there were results check if the passwords match and if they do fill a User object with the data
+        //stored in the database
+        if(query_result.isBeforeFirst()){
+            query_result.next();
+            if(query_result.getString("password").equals(login_user.password)){
+                User user_data = new User();
+                user_data.id = query_result.getInt("id");
+                user_data.username = query_result.getString("username");
+                user_data.name = query_result.getString("nome");
+                user_data.password = query_result.getString("password");
+                user_data.administrator = query_result.getBoolean("administrador");
+
+                query_result.updateBoolean("autenticado", true );
+
+                user_data.authenticated = query_result.getBoolean("autenticado");
+
+                return new Message(Message.TYPE_OF_MESSAGE.LOGIN,new UserViewModel(user_data,Error_Messages.SUCESS));
+            }
+
+            query_result.updateBoolean("autenticado", false );
+            return new Message(Message.TYPE_OF_MESSAGE.LOGIN, new UserViewModel(null,Error_Messages.INVALID_PASSWORD));
+
+
+        }
+
+        query_result.updateBoolean("autenticado", false );
+        return new Message(Message.TYPE_OF_MESSAGE.LOGIN, new UserViewModel(null,Error_Messages.USERNAME_DOESNT_EXIST));
+
+
 
     }
 
