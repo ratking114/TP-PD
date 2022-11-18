b73aaabe622236a8b9b5a0c9f58c2acca44fb6b3
noslen-dev <79165069+noslen-dev@users.noreply.github.com>
2022-11-18 12:14:15 +0000

    Add files via upload

---
+++ src/server/ServerClasses/Prepare.java
@@ -0,0 +1,28 @@
+package server.ServerClasses;
+
+import java.io.Serializable;
+
+/**
+ * Prepare is the message that is sent when a Server needs to update its server.database.
+ */
+public class Prepare implements Serializable {
+    public Prepare(String query, int prepare_number){
+        this.query = query;
+        this.prepare_number = prepare_number;
+    }
+
+    /**
+     * The number that identifies this Prepare message
+     */
+    public int prepare_number;
+
+    /**
+     * The query that must be run to update the server.database.
+     */
+    public String query;
+
+    /**
+     * The udp port number where the sender will wait for confirmation messages.
+     */
+    public int answer_udp_port;
+}
